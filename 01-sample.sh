d0937e49b304a695024c76f095be7b0417e4b422
shiva shankar <sivas1475@gmail.com>
2022-04-29 18:51:56 +0530

    Adding comments

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 ls
 # This is a comment line
+# comments are generally used for
+# 1. Adding description to the code
+# 2. commenting some piece of code that is not needed to run
+
