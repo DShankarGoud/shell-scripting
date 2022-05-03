48db4b3c4bd1d44368faf46541d3255dd6506eb5
shiva shankar <sivas1475@gmail.com>
2022-05-03 17:23:45 +0530

    Special Variables

--- 05-Special-vars.sh
+++ 05-Special-vars.sh
@@ -2,5 +2,11 @@
 
 # Special Variables are $0-$n , $* , $@ , $#
 
+echo Script Name = $0
+
 echo Fisrt Argument = $1
 echo Second Argument = $2
+
+echo All Arguments = $*
+
+echo no of Arguments = $#
